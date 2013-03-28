e12d771cc05043662f4ae5fc941b62ae327c4ba7
Start <4544242@qq.com>
2013-03-28 22:33:55 +0800

    Revert "Revert "a""
    This reverts commit 796a696b7c75141b654d4e0ea4a36c88a8557924.

---
+++ fourinone/test/com/demo2/ParkServerDemo.java
@@ -0,0 +1,11 @@
+package com.demo2;
+
+import com.fourinone.BeanContext;
+
+public class ParkServerDemo
+{
+	public static void main(String[] args)
+	{
+		BeanContext.startPark();
+	}
+}
---
+++ fourinone/test/com/demo7/ParkServerDemo.java
@@ -0,0 +1,11 @@
+package com.demo7;
+
+import com.fourinone.BeanContext;
+
+public class ParkServerDemo
+{
+	public static void main(String[] args)
+	{
+		BeanContext.startPark();
+	}
+}
